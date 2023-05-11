22a5338dd146e93cc8c40b7883fede31568ff9dc
Lapis <76179088+LapisHusky@users.noreply.github.com>
2023-05-11 11:24:56 -0700

    Update main.js

--- packages/polybius/main.js
+++ packages/polybius/main.js
@@ -1,6 +1,6 @@
 (function() {
 	function install() {
-		alert("Polybius is not available yet. If you know how to get it working, please make a pull request here: https://github.com/LapisHusky/betteropm/")
+		alert("polybius is not available yet. If you know how to get it working, please make a pull request here: https://github.com/LapisHusky/betteropm/")
 	}
 	
 	function uninstall() {
