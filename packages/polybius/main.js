ae7230121de75518a82936182c5b54dc16d23aee
Lapis <76179088+LapisHusky@users.noreply.github.com>
2023-05-11 11:24:14 -0700

    Update and rename placeholder to main.js

---
+++ packages/polybius/main.js
@@ -0,0 +1,14 @@
+(function() {
+	function install() {
+		alert("Polybius is not available yet. If you know how to get it working, please make a pull request here: https://github.com/LapisHusky/betteropm/")
+	}
+	
+	function uninstall() {
+		
+	}
+	
+	return {
+		install: install,
+		uninstall: uninstall
+	};
+})()
